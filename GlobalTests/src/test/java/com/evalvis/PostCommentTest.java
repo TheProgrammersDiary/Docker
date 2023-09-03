ba15dcb7b6fbb09df18832e39a66f859d8544400
Evaldas Visockas <vievaldas@gmail.com>
2023-09-03 22:22:23 +0300

    Add first E2E test

--- GlobalTests/src/test/java/com/evalvis/PostCommentTest.java
+++ GlobalTests/src/test/java/com/evalvis/PostCommentTest.java
@@ -1,17 +1,31 @@
 package com.evalvis;
 
-import org.hamcrest.Matchers;
+import au.com.origin.snapshots.Expect;
+import au.com.origin.snapshots.annotations.SnapshotName;
+import au.com.origin.snapshots.junit5.SnapshotExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.node.ArrayNode;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.node.ObjectNode;
+import protobufs.CommentRequest;
+import protobufs.PostRequest;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
-import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.*;
+import static shadow.org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Testcontainers
+@ExtendWith({SnapshotExtension.class})
 public class PostCommentTest {
+
+    private Expect expect;
 
     @Container
     private static final DockerComposeContainer<?> dockerComposeContainer =
@@ -24,11 +38,60 @@ public class PostCommentTest {
                     .withExposedService("post", 8080);
 
     @Test
-    public void serviceResponds() {
-        get("http://localhost:8080/comments/list-comments/1")
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("", Matchers.hasSize(0));
+    @SnapshotName("createsPostWithComments")
+    public void createsPostWithComments() throws IOException {
+        PostRequest postRequest = PostRequest
+                .newBuilder()
+                .setAuthor("Human")
+                .setTitle("Testing matters")
+                .setContent("You either test first, test along coding, or don't test at all.")
+                .build();
+
+        String postId = given()
+                .baseUri("http://localhost:8081")
+                .body(postRequest.toString())
+                .when()
+                .post("/posts/create")
+                .getBody()
+                .jsonPath()
+                .get("id")
+                .toString();
+        int commentCount = 2;
+        String[] commentIds = new String[commentCount];
+        for(int i = 0; i < commentCount; i++) {
+            commentIds[i] = given()
+                    .baseUri("http://localhost:8080")
+                    .body(
+                            CommentRequest
+                                    .newBuilder()
+                                    .setAuthor("author" + i)
+                                    .setContent("content" + i)
+                                    .setPostId(postId)
+                            .build().toString()
+                    )
+                    .post("/comments/create")
+                    .getBody()
+                    .jsonPath()
+                    .get("id");
+        }
+        ArrayNode comments = (ArrayNode) new ObjectMapper().readTree(
+                get("http://localhost:8080/comments/list-comments/" + postId)
+                        .getBody()
+                        .asString()
+        );
+        assertThat(comments.size()).isEqualTo(commentCount);
+        for(int i = 0; i < comments.size(); i++) {
+            assertThat(comments.get(i).get("id").textValue()).isEqualTo(commentIds[i]);
+        }
+        maskProperties(comments, "id", "postEntryId");
+        expect.toMatchSnapshot(comments.toString());
+    }
+
+    private void maskProperties(ArrayNode node, String... properties) {
+        node.forEach(element ->
+                Arrays
+                        .stream(properties)
+                        .forEach(property -> ((ObjectNode) element).put(property, "#hidden#"))
+        );
     }
 }
