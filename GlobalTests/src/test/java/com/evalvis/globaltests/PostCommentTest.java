b879ad1745de7cc07ff2294e0509d43eab1b39fc
Evaldas Visockas <vievaldas@gmail.com>
2023-09-23 20:48:23 +0300

    Instead of testcontainers, use docker compose

--- GlobalTests/src/test/java/com/evalvis/globaltests/PostCommentTest.java
+++ GlobalTests/src/test/java/com/evalvis/globaltests/PostCommentTest.java
@@ -5,35 +5,20 @@ import au.com.origin.snapshots.annotations.SnapshotName;
 import au.com.origin.snapshots.junit5.SnapshotExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.node.ArrayNode;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.node.ObjectNode;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
 import static io.restassured.RestAssured.*;
 import static shadow.org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@Testcontainers
 @ExtendWith({SnapshotExtension.class})
 public class PostCommentTest {
 
     private Expect expect;
-
-    @Container
-    private static final DockerComposeContainer<?> dockerComposeContainer =
-            new DockerComposeContainer<>(
-                    new File("src/test/resources/docker-compose-test.yaml")
-            )
-                    .withExposedService("postgres", 5432)
-                    .withExposedService("mongodb", 27017)
-                    .withExposedService("blog", 8080)
-                    .withExposedService("post", 8080);
 
     @Test
     @SnapshotName("createsPostWithComments")
