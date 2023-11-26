daa1c8f56a7520614bec21177f7b85a932fb396d
Evaldas Visockas <vievaldas@gmail.com>
2023-11-26 19:28:25 +0200

    Fix failing test

--- GlobalTests/src/test/java/com/evalvis/globaltests/PostCommentTest.java
+++ GlobalTests/src/test/java/com/evalvis/globaltests/PostCommentTest.java
@@ -6,6 +6,7 @@ import au.com.origin.snapshots.junit5.SnapshotExtension;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.restassured.http.Cookie;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -23,6 +24,8 @@ public class PostCommentTest {
     @Test
     @SnapshotName("createsPostWithComments")
     public void createsPostWithComments() throws IOException {
+        signUp();
+        Cookie jwt = login();
         String postId = given()
                 .baseUri("http://localhost:8081")
                 .contentType("application/json")
@@ -57,22 +60,58 @@ public class PostCommentTest {
                                     .put("postId", postId)
                                     .toString()
                     )
+                    .cookie(jwt)
                     .post("/comments/create")
                     .getBody()
                     .jsonPath()
                     .get("id");
         }
-        ArrayNode comments = (ArrayNode) new ObjectMapper().readTree(
-                get("http://localhost:8080/comments/list-comments/" + postId)
-                        .getBody()
-                        .asString()
-        );
+        ArrayNode comments = (ArrayNode) new ObjectMapper()
+                .readTree(
+                        given()
+                                .baseUri("http://localhost:8080")
+                                .contentType("application/json")
+                                .cookie(jwt)
+                                .get("/comments/list-comments/" + postId)
+                                .getBody()
+                                .asString()
+                );
         assertThat(comments.size()).isEqualTo(commentCount);
         for(int i = 0; i < comments.size(); i++) {
             assertThat(comments.get(i).get("id").textValue()).isEqualTo(commentIds[i]);
         }
         maskProperties(comments, "id", "postEntryId");
         expect.toMatchSnapshot(comments.toString());
+    }
+
+    private void signUp() {
+        given()
+                .baseUri("http://localhost:8080")
+                .contentType("application/json")
+                .body(
+                        new ObjectMapper()
+                                .createObjectNode()
+                                .put("username", "testuser")
+                                .put("password", "test")
+                                .put("email", "testuser@test.com")
+                                .toString()
+                )
+                .post("/users/signup");
+    }
+
+    private Cookie login() {
+        return given()
+                .baseUri("http://localhost:8080")
+                .contentType("application/json")
+                .body(
+                        new ObjectMapper()
+                                .createObjectNode()
+                                .put("username", "testuser")
+                                .put("password", "test")
+                                .toString()
+                )
+                .post("/users/login")
+                .getDetailedCookie("jwt");
     }
 
     private void maskProperties(ArrayNode node, String... properties) {
